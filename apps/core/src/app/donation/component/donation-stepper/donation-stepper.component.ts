2d6a407216c2ce39eb5a9febbc5140a5d59f81cc
fmalessio <fmalessio@gmail.com>
2020-09-27 02:09:17 -0300

    Add State Stepper

--- apps/core/src/app/donation/component/donation-stepper/donation-stepper.component.ts
+++ apps/core/src/app/donation/component/donation-stepper/donation-stepper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Components } from 'state-stepper/loader';
 
 @Component({
   selector: 'app-donation-stepper',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonationStepperComponent implements OnInit {
 
-  constructor() { }
+  size: string = 'medium';
+  states: Map<number, STEPPER_STATE> = new Map<number, STEPPER_STATE>();
+  private stateStepper: Components.StateStepper;
+  private currentPosition: number = 0;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.stateStepper = document.querySelector('state-stepper');
+    this.stateStepper.resetSteps();
+  }
+
+  next(): void {
+    this.stateStepper.stepNext(STEPPER_STATE.SUCCESS);
+    this.currentPosition += 1;
+    console.log(this.currentPosition);
+  }
+
+  back(): void {
+    this.stateStepper.stepBack();
+    this.currentPosition -= 1;
+    console.log(this.currentPosition);
+  }
+
+  reset(): void {
+    this.stateStepper.resetSteps();
+  }
+
+  move(positionClicked: number): void {
+    if (positionClicked !== this.currentPosition) {
+      (positionClicked < this.currentPosition) ? this.back() : this.next();
+    }
+  }
+
+}
+
+const enum STEPPER_STATE {
+  ACTIVE = 'active',
+  WARN = 'warn',
+  SUCCESS = 'success',
+  DANGER = 'danger'
 }
