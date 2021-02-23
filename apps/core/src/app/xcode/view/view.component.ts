f96fb5a2c3a5ffec32d906f0c624beaa9967d413
fmalessio <fmalessio@gmail.com>
2021-02-23 00:54:04 -0300

    Print QR code in PDF

--- apps/core/src/app/xcode/view/view.component.ts
+++ apps/core/src/app/xcode/view/view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-view',
@@ -15,7 +16,25 @@ export class ViewComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.value = this.activatedRoute.snapshot.paramMap.get('value');
+    const plainValue = this.activatedRoute.snapshot.paramMap.get('value');
+    this.value = btoa(plainValue);
+  }
+
+  printPDF() {
+    var canvas = document.getElementById('qrdatacode')?.getElementsByTagName('canvas')[0];
+    if (!canvas) {
+      console.error("Canvas not found");
+      return;
+    }
+    var dataURL = canvas.toDataURL();
+    let doc = new jsPDF();
+    doc.setProperties({ author: 'Collavorative World' });
+    doc.setFont("helvetica");
+    doc.setFontSize(9);
+    doc.addImage(dataURL, 'JPG', 10, 10, 60, 60)
+      .text(btoa(this.value), 10, 70)
+      .text(new Date().toJSON().slice(0, 10).replace(/-/g, '/'), 10, 75);
+    doc.save(`cw-${Date.now()}.pdf`);
   }
 
 }
