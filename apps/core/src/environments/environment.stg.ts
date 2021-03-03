8e2b13e586461bef3d998f224f342d5e9c8e86bf
fmalessio <fmalessio@gmail.com>
2021-03-03 00:21:43 -0300

    Add stg environment

---
+++ apps/core/src/environments/environment.stg.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+import { secretEnv } from "./environment.secret";
+
+export const environment = {
+  production: false,
+  endpoint: 'https://collaborative-world-service.herokuapp.com',
+  googleMapsKey: secretEnv.GOOGLE_MAPS_KEY
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
